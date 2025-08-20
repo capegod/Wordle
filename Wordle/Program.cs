854e71426d25e9549be65a9ceb4a411c9157dc9a
capegod <hnrq.mirandas@gmail.com>
2025-08-19 21:02:54 -0300

    Atualização do código correto.

--- Wordle/Program.cs
+++ Wordle/Program.cs
@@ -1,8 +1,6 @@
 ﻿using Spectre.Console;
-using Spectre.Console.Rendering;
 using System;
 using System.Reflection;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.RegularExpressions;
 
